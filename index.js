3ae8edefa85b24e9228bdb1522596023aa881a2b
RoyalDeveloper2015 <top.webdeveloper2@gmail.com>
2017-10-06 01:14:04 +1000

    C

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 
 app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers",'Content-Type');
   next();
 })
 
